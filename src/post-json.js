27ae359b34ade7c3f5edb368ec0da05b9338366c
Adrian <adrian@lawcpd.com.au>
2019-08-28 09:19:20 +0800

    No default parameters :(. Case 5861

--- src/post-json.js
+++ src/post-json.js
@@ -1,10 +1,12 @@
-function postJson(url, body, headers = {}) {
+function postJson(url, body, headers) {
   return new Promise(function(resolve, reject) {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', url);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    for (var name in headers) {
-      xhr.setRequestHeader(name, headers[name]);
+    if (headers) {
+      for (var name in headers) {
+        xhr.setRequestHeader(name, headers[name]);
+      }
     }
     xhr.onreadystatechange = function() {
       if (this.readyState === 4) {
